3a6d54c43aa64c54c58132cae9002c4c58fd4135
zhangsan <zhangsan@itcast.cn>
2019-06-10 21:08:50 +0800

    修改gitTest

--- src/test/java/com/itheima/test/GitTest.java
+++ src/test/java/com/itheima/test/GitTest.java
@@ -7,11 +7,15 @@ public class GitTest {
     @Test
     public void test1(){
         System.out.println("hello,git");
+        System.out.println("我很烦");
         System.out.println(".....................");
+
     }
 
     @Test
     public void test2(){
+
+        //循环输出........
         for (int i = 0; i < 3; i++) {
 
             System.out.println("hello,git");
