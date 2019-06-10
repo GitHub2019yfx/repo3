f4de816b9f73a373e12c0f52124e07fcc34baa67
zhangsan <zhangsan@itcast.cn>
2019-06-10 20:42:03 +0800

    添加一个项目到git////

---
+++ src/test/java/com/itheima/test/GitTest.java
@@ -0,0 +1,21 @@
+package com.itheima.test;
+
+import org.junit.Test;
+
+public class GitTest {
+
+    @Test
+    public void test1(){
+        System.out.println("hello,git");
+        System.out.println(".....................");
+    }
+
+    @Test
+    public void test2(){
+        for (int i = 0; i < 3; i++) {
+
+            System.out.println("hello,git");
+        }
+    }
+
+}
